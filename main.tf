80988eaf9737d382ddd987aa4ced5ef0913fd167
joebeone <jbennett@webrapids.com>
2022-03-01 14:33:48 -0500

    typo

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_s3_bucket" "s3_bucket" {
 resource "aws_s3_bucket_public_access_block" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
-  blockblock_public_acls  = true
+  block_public_acls  = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
